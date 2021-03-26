b4eeb53699c598194560e4833aee92492ad36f4f
Emeka Boris Ama <borisphilosophy@gmail.com>
2021-03-26 01:37:35 +0000

    fix deploy

--- mergedata.py
+++
@@ -1,9 +0,0 @@
-
-
-
-filenames = ['file1.txt', 'file2.txt', ...]
-with open('./data/', 'w') as outfile:
-    for fname in filenames:
-        with open(fname) as infile:
-            for line in infile:
-                outfile.write(line)
